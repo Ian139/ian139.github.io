9cd9e6e72939ef7020e9caa67f3bcbed6f819258
ian139 <58665692+ian139@users.noreply.github.com>
2022-08-19 14:03:46 -0400

    Errors with /scores

--- scores.js
+++ scores.js
@@ -3,7 +3,6 @@ let click = document.querySelector(".clicky");
 let scoreList = document.querySelector(".scoreList");
 
 function view() {
-	let localScore = localStorage.getItem("score");
 	if (localScore != null) {
 		scoreList.innerHTML = JSON.parse(localScore);
 	}
